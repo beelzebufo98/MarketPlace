90cf3144d10a8c5c06fb74ab897a1876409a0dac
EVGHENII GRINCENCO <130862070+beelzebufo98@users.noreply.github.com>
2025-06-08 14:09:48 +0300

    Initial commit

---
+++ OrdersService/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersService.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class OrderController : ControllerBase
+  {
+  }
+}
---
+++ PaymentsService/Controllers/PaymentController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentsService.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class PaymentController : ControllerBase
+  {
+  }
+}
